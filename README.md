3510c62e2a8a75f041adbdfcf89f8f43114ae84e
Bill Tomczak <bill@joomlashack.com>
2020-07-29 11:21:57 -0700

    Initialize using AllediaFramework v1.6.4

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Joomlashack](https://www.joomlashack.com/images/logo_circle_small.png)](https://www.joomlashack.com)
+
+Joomlashack Framework
+======================
+
+Shared libraries for extensions development
---
+++ tests/_support/mock/com_dumbextension_pro/library/Pro/ProLib.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package   AllediaFramework
+ * @contact   www.alledia.com, hello@alledia.com
+ * @copyright 2016 Alledia.com, All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+namespace Alledia\DumbExtension\Pro;
+
+
+use Alledia\DumbExtension\Free\FreeLib;
+
+
+class ProLib extends FreeLib
+{
+
+}
